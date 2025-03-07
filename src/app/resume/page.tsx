1198686e58373c33538556bee35c1d88635dc141
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-03-07 20:33:36 +0300

    add css for the abut page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,15 @@
-'use client'
+'use client';
+
 import React from 'react';
 import './resume.css';
+import { useTheme } from '@/context/ThemeContext'; // Import the useTheme hook
 
 export default function Resume() {
+  const { theme } = useTheme(); // Access the theme state
+
   return (
-    <section id="resume" className="resume section">
+    
+    <section id="resume" className={`resume section ${theme === 'dark' ? 'dark' : 'light'}`}>
       <div className="container">
         <h2>Resume</h2>
         <div className="pdf-container">
