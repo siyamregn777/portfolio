3f2c4269e1a751a2f62477a0b716e41585657bd0
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-04-07 12:11:13 +0300

    refactor about section styles and content; enhance layout, improve readability, and update skill descriptions

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,19 +2,16 @@
 
 import { FaCode, FaServer, FaRobot, FaTools } from 'react-icons/fa';
 import './about.css';
-import { useTheme } from '@/context/ThemeContext'; // Import the useTheme hook
 
 export default function About() {
-  const { theme } = useTheme(); // Access the theme state
-
   return (
-    <section id="about" className={`about section ${theme === 'dark' ? 'dark' : 'light'}`}>
+    <section id="about" className="about section">
       <div className="container">
-        <h2 className="about-heading">About Me</h2>
+        <h2 className="section-title">About Me</h2>
         <div className="about-content">
           <div className="about-text">
             <p>
-              I’m a passionate <strong>Full-Stack Developer</strong> with expertise in modern web technologies, 
+              I`m a passionate <strong>Full-Stack Developer</strong> with expertise in modern web technologies, 
               backend development, and AI-driven solutions. With a strong foundation in 
               JavaScript, TypeScript, React, Next.js, and Node.js, I build high-performance, 
               scalable, and user-friendly applications.
@@ -22,7 +19,7 @@ export default function About() {
             <p>
               Beyond coding, I have experience in virtual assistance, ensuring efficiency 
               and organization for clients. I am detail-oriented, always eager to learn, 
-              and committed to delivering top-notch solutions. Let’s collaborate and bring 
+              and committed to delivering top-notch solutions. Let`s collaborate and bring 
               your ideas to life!
             </p>
           </div>
@@ -30,7 +27,7 @@ export default function About() {
             <div className="skill-card">
               <FaCode className="skill-icon" />
               <h3>Frontend</h3>
-              <p>React, Next.js, HTML, CSS,   Vue.js,   JavaScript, TypeScript</p>
+              <p>React, Next.js, HTML, CSS, Vue.js, JavaScript, TypeScript</p>
             </div>
             <div className="skill-card">
               <FaServer className="skill-icon" />
@@ -45,7 +42,7 @@ export default function About() {
             <div className="skill-card">
               <FaTools className="skill-icon" />
               <h3>Tools</h3>
-              <p>Git, Docker, VS Code, Postman ,Apidog</p>
+              <p>Git, Docker, VS Code, Postman, Apidog</p>
             </div>
           </div>
         </div>
